113578fa15812de7123de57b6f48ac7dac04b3a4
Josh Gavant <joshgavant@gmail.com>
2018-09-25 21:31:05 -0700

    add minimal Razor frontend

--- HttpServer.cs
+++ HttpServer.cs
@@ -12,9 +12,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace ProfilesApp {
-    public class Server {
-
+namespace ProfilesApp
+{
+    public class Server
+    {
         private const string nameClaimType = "name";
         private const string preferredUsernameClaimType = "preferred_username";
         private const string oidClaimType = "http://schemas.microsoft.com/identity/claims/objectidentifier";
@@ -22,47 +23,80 @@ namespace ProfilesApp {
         private const string nameIdentifierClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
         private const string emailClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
 
-        // implements Microsoft.AspNetCore.Http.RequestDelegate
-        public static async Task DefaultRequestDelegate(HttpContext context) {
+        public static async Task DefaultRequestDelegate(HttpContext context)
+        {
             await context.Response.WriteAsync("Hello!");
         }
 
-        // implements Microsoft.AspNetCore.Http.RequestDelegate
-        public static async Task RenderProfilePage(HttpContext context) {
-            ClaimsPrincipal principal;
+        public static async Task RenderProfilePage(HttpContext context)
+        {
+            var renderer = context.RequestServices.GetRequiredService<IViewRenderService>();
+            UserProfile profile = await GetUserProfileFromContext(context);
+            // await context.Response.WriteAsync($"Hi {profile.FullName}. You've been authenticated with identifier {profile.Id}.");
+            await context.Response.WriteAsync(
+                await renderer.RenderViewToStringAsync("Profile", profile));
+        }
+
+        public static async Task PostProfileForm(HttpContext context)
+        {
+            var principal = (ClaimsPrincipal)context.User;
+            var idClaim = principal.FindFirst(nameIdentifierClaimType);
+            var formData = context.Request.Form;
+            var updatedProfile = new UserProfile() {
+                Id = idClaim.Value,
+                Etag = formData["etag"],
+                FullName = formData["fullName"],
+                Email = formData["email"],
+            };
+
+            // check for picture and upload to blob store
+            if (formData.Files.Any()) {
+                var stream = formData.Files.First().OpenReadStream();
+                var pictures = new Pictures();
+                var pictureUrl = await pictures.PutPicture(updatedProfile.Id, stream);
+                updatedProfile.PictureUrl = await pictures.GetPictureUrl(updatedProfile.Id);
+            }
+
+            var profiles = new UserProfiles();
+            await profiles.UpdateProfileAsync(updatedProfile);
+            await RenderProfilePage(context);
+            return;
+        }
+
+        public static async Task<UserProfile> GetUserProfileFromContext(HttpContext context)
+        {
             var logger = context.RequestServices.GetRequiredService<ILogger<IWebHost>>();
 
             if (!context.User.Identity.IsAuthenticated) {
                 await context.ChallengeAsync();
-                return;
+								return new UserProfile();
             }
 
-            principal = context.User as ClaimsPrincipal;
-            if (null != principal) {
-                var name = principal.FindFirst("name");
-                logger.LogInformation($"name claim: [{name}]");
-                await context.Response.WriteAsync($"Hi {name.Value}! You've been authenticated.");
-                foreach (Claim claim in principal.Claims) {
-                    logger.LogInformation($"claim: [{claim}]");
-                }
-            }
-
+            ClaimsPrincipal principal = (ClaimsPrincipal)context.User;
             var idClaim = principal.FindFirst(nameIdentifierClaimType);
+            var nameClaim = principal.FindFirst(nameClaimType);
+            logger.LogInformation($"[{nameClaim}, {idClaim}]");
+            foreach (Claim claim in principal.Claims) {
+                logger.LogInformation($"claim: [{claim}]");
+            }
 
-            var profiles = new UserProfilesCollection();
-            await profiles.AddProfileAsync(new UserProfile() {
-                Id = idClaim.Value,
-                FullName = principal.FindFirst(nameClaimType).Value,
-                Email = principal.FindFirst(emailClaimType).Value,
-                ExternalIdentifier = idClaim.Value,
-                Etag = ""
-            });
+            var profiles = new UserProfiles();
+            // first see if profile exists for this ID
+            var profile = await profiles.GetProfileAsync(idClaim.Value);
 
-            var currentProfile = await profiles.GetProfileAsync(idClaim.Value);
-            logger.LogInformation($"got profileId {currentProfile.Id}, fullName {currentProfile.FullName}");
+            // if not, add a new profile for this ID
+            if (profile == null) {
+                await profiles.AddProfileAsync(new UserProfile() {
+                    Id = idClaim.Value,
+                    FullName = principal.FindFirst(nameClaimType).Value,
+                    Email = principal.FindFirst(emailClaimType).Value,
+                    ExternalIdentifier = idClaim.Value 
+                });
+                profile = await profiles.GetProfileAsync(idClaim.Value);
+            }; 
 
-            return;
-
+            logger.LogInformation($"got profileId {profile.Id}, fullName {profile.FullName}");
+            return profile;
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -8,17 +11,20 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ObjectPool;
 
 namespace ProfilesApp
 {
-
     public static class Configuration
     {
         private static IConfiguration _configuration;
@@ -41,6 +47,8 @@ namespace ProfilesApp
         public void ConfigureServices(IServiceCollection services)
         {
             // router
+						ConfigureRenderingServices(services, "");
+            services.AddScoped<IViewRenderService, ViewRenderService>();
             services.AddRouting();
 						services.AddAuthentication(options => {
 								options.DefaultAuthenticateScheme =
@@ -81,11 +89,44 @@ namespace ProfilesApp
             // default handler
             var routeBuilder = new RouteBuilder(app, new RouteHandler(Server.DefaultRequestDelegate));
             routeBuilder.MapGet("hello", Server.RenderProfilePage);
+						routeBuilder.MapPost("hello", Server.PostProfileForm);
             routeBuilder.MapRoute("root", "/");
 
             var routes = routeBuilder.Build();
             app.UseAuthentication();
             app.UseRouter(routes);
+        }
+
+        private static void ConfigureRenderingServices(IServiceCollection services, string customApplicationBasePath)
+        {
+            string applicationName;
+            IFileProvider fileProvider;
+            if (!string.IsNullOrEmpty(customApplicationBasePath))
+            {
+                applicationName = Path.GetFileName(customApplicationBasePath);
+                fileProvider = new PhysicalFileProvider(customApplicationBasePath);
+            }
+            else
+            {
+                applicationName = Assembly.GetEntryAssembly().GetName().Name;
+                fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+            }
+
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                ApplicationName =  applicationName,
+                WebRootFileProvider = fileProvider,
+            });
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
+            });
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+            services.AddLogging();
+            services.AddMvc();
         }
     }
 }
